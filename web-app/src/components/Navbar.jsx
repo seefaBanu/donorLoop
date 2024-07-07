f66e97b0eb1954f19e04eedf8c97d3a74365c619
seefaBanu <banuags.20@uom.lk>
2024-07-07 21:59:15 +0530

    navbar responsive for mobile

--- web-app/src/components/Navbar.jsx
+++ web-app/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Logo from "../assets/logo.png";
-import { IoIosNotifications } from "react-icons/io";
+import { IoIosNotifications, IoMdClock, IoMdClose, IoMdMenu } from "react-icons/io";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { useAuthContext } from "@asgardeo/auth-react";
 import { useNavigate } from "react-router-dom";
+import { IoLogOut } from "react-icons/io5";
 
 export default function Navbar() {
   const [group, setGroup] = useState([]);
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [sideMenuOpen, setSideMenuOpen] = useState(false);
   const [userDetails, setUserDetails] = useState({});
   const { state, signIn, signOut, getBasicUserInfo } = useAuthContext();
   const navigate = useNavigate();
@@ -16,7 +18,10 @@ export default function Navbar() {
     setDropdownOpen(!dropdownOpen);
   };
 
-  // Helper function to truncate name if it's longer than 10 characters
+  const toggleSideMenu = () => {
+    setSideMenuOpen(!sideMenuOpen);
+  };
+
   const getTruncatedName = (name) => {
     return name && name.length > 10 ? name.substring(0, 10) + "..." : name;
   };
@@ -25,7 +30,7 @@ export default function Navbar() {
     if (state.isAuthenticated) {
       getBasicUserInfo().then((response) => {
         if (response) {
-          setGroup(response.groups || []); // Ensure groups is an array
+          setGroup(response.groups || []);
           setUserDetails(response);
         } else {
           setGroup([]);
@@ -36,65 +41,90 @@ export default function Navbar() {
   }, [state]);
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 grid grid-cols-3 bg-gradient-to-r from-red-900 to-red-600 h-20 text-black py-4 px-8 shadow-md">
+    <div className="fixed top-0 left-0 right-0 z-50 flex flex-row bg-gradient-to-r from-red-900 to-red-600 h-20 text-black py-4 px-8 shadow-md">
       <div className="flex justify-start items-center">
         <div className="bg-white rounded-3xl">
           <img src={Logo} alt="Logo" className="h-10 w-10 " />
         </div>
-        <h1 className="font-bold text-white px-2 sm:not show">DONOR LOOP</h1>
+        <h1 className="font-bold text-white px-2 sm:hidden">DONOR LOOP</h1>
       </div>
 
       {state.isAuthenticated ? (
         <>
-          <div className="flex grow justify-center items-center">
-            <p className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105" onClick = {() => navigate("/camps")} >Camps</p>
-            {group.includes("blood_bank") && (
-              <p className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105" onClick={()=> navigate("/find-blood")}>Find Blood</p>
-            )}
-            <p className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105" onClick = {() => navigate("/request")}>Requests</p>
-          </div>
-
-          <div className="flex justify-end items-center px-2">
-            <IoIosNotifications className="text-white mx-2" />
-            <div className="flex bg-white rounded-full h-12 items-center justify-between px-2 relative">
-              <div className="bg-white rounded-full mx-2 h-8 w-8 flex items-center justify-center shadow-lg">
-                <img
-                  src={userDetails.picture || Logo} // Default to Logo if picture is not available
-                  alt="user"
-                  className="h-6 w-6 rounded-full"
-                />
-              </div>
-              <div className="w-24">
-                <p className="text-sm text-black font-bold">
-                  {getTruncatedName(userDetails.displayName || "No Name")} {/* Default to "No Name" if displayName is not available */}
+          <div className="flex flex-row w-full justify-between ">
+            <div className="flex grow justify-center items-center sm:hidden sm:w-0 ">
+              <p
+                className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105"
+                onClick={() => navigate("/camps")}
+              >
+                Camps
+              </p>
+              {group.includes("blood_bank") && (
+                <p
+                  className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105"
+                  onClick={() => navigate("/find-blood")}
+                >
+                  Find Blood
                 </p>
-                {group.includes("blood_bank") ? (
-                  <p className="text-xs text-black">Blood Bank</p>
-                ) : (
-                  <p className="text-xs text-black">Donor</p>
+              )}
+              <p
+                className="text-xs text-white px-2 hover:cursor-pointer hover:scale-105"
+                onClick={() => navigate("/request")}
+              >
+                Requests
+              </p>
+            </div>
+
+            <div className="flex justify-end items-center px-2 sm:hidden sm:w-0 ">
+              <IoIosNotifications className="text-white mx-2" />
+              <div className="flex bg-white rounded-full h-12 items-center justify-between px-2 relative">
+                <div className="bg-white rounded-full mx-2 h-8 w-8 flex items-center justify-center shadow-lg">
+                  <img
+                    src={userDetails.picture || Logo} // Default to Logo if picture is not available
+                    alt="user"
+                    className="h-6 w-6 rounded-full"
+                  />
+                </div>
+                <div className="w-24">
+                  <p className="text-sm text-black font-bold">
+                    {getTruncatedName(userDetails.displayName || "No Name")}{" "}
+                    {/* Default to "No Name" if displayName is not available */}
+                  </p>
+                  {group.includes("blood_bank") ? (
+                    <p className="text-xs text-black">Blood Bank</p>
+                  ) : (
+                    <p className="text-xs text-black">Donor</p>
+                  )}
+                </div>
+                <IoMdArrowDropdown
+                  className="cursor-pointer"
+                  onClick={toggleDropdown}
+                />
+                {dropdownOpen && (
+                  <div className="absolute right-0 mt-12 bg-white border rounded shadow-lg w-32">
+                    <button
+                      className="w-full text-left px-4 py-2 text-sm hover:bg-gray-200"
+                      onClick={() => signOut()}
+                    >
+                      Logout
+                    </button>
+                  </div>
                 )}
               </div>
-              <IoMdArrowDropdown
-                className="cursor-pointer"
-                onClick={toggleDropdown}
-              />
-              {dropdownOpen && (
-                <div className="absolute right-0 mt-12 bg-white border rounded shadow-lg w-32">
-                  <button
-                    className="w-full text-left px-4 py-2 text-sm hover:bg-gray-200"
-                    onClick={() => signOut()}
-                  >
-                    Logout
-                  </button>
-                </div>
-              )}
             </div>
+            <div className="hidden sm:w-full sm:flex sm:justify-end sm:items-center">
+            <IoMdMenu
+              className="text-white text-2xl cursor-pointer sm:text-right"
+              onClick={toggleSideMenu}
+            />
+          </div>{" "}
           </div>
+         
         </>
       ) : (
         <>
           <div></div>
-          <div className="flex justify-end items-center px-2 gap-2">
+          <div className="flex justify-end items-center px-2 gap-4 w-full">
             <button
               className="border-black border text-white font-light px-6 py-2 rounded-full shadow-lg hover:bg-white hover:text-black transition duration-300"
               onClick={() => signIn()}
@@ -103,6 +133,55 @@ export default function Navbar() {
             </button>
           </div>
         </>
+      )}
+       {sideMenuOpen && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 z-40" onClick={toggleSideMenu}>
+          <div className="fixed top-0 right-0 bottom-0 w-64 bg-white shadow-lg z-50 " onClick={(e) => e.stopPropagation()}>
+            <div className="flex items-center justify-between  border-b-2">
+              <div className="flex flex-row w-full px-2 py-5 justify-between items-center">
+              <div className="flex items-center gap-2  ">
+                <div className="bg-white rounded-full h-8 w-8 flex items-center justify-center shadow-lg">
+                  <img
+                    src={userDetails.picture || Logo} // Default to Logo if picture is not available
+                    alt="user"
+                    className="h-6 w-6 rounded-full flex-end"
+                  />
+                </div>
+                <div>
+                  <p className="text-sm text-black font-bold">
+                    {getTruncatedName(userDetails.displayName || "No Name")} {/* Default to "No Name" if displayName is not available */}
+                  </p>
+                  {group.includes("blood_bank") ? (
+                    <p className="text-sm text-black">Blood Bank</p>
+                  ) : (
+                    <p className="text-sm text-black">Donor</p>
+                  )}
+                </div>
+              </div>
+              <IoMdClose className="flex text-2xl bg-gray-300 cursor-pointer justify-end " onClick={toggleSideMenu} />
+              </div>
+            </div>
+
+            <div className="mb-4 ">
+              <p className="text-base p-4 text-black cursor-pointer border-b-2 hover:bg-black hover:text-white transition duration-300" onClick={() => navigate("/camps")}>Camps</p>
+              {group.includes("blood_bank") && (
+                <p className="text-base p-4 text-black cursor-pointer border-b-2 hover:bg-black hover:text-white  transition duration-300" onClick={() => navigate("/find-blood")}>Find Blood</p>
+              )}
+              <p className="text-base p-4 text-black cursor-pointer border-b-2 hover:bg-black hover:text-white transition duration-300" onClick={() => navigate("/request")}>Requests</p>
+            </div>
+
+            <button
+              className="w-full flex text-left p-4 text-base hover:bg-gray-300  mt-4"
+              onClick={() => {
+                signOut();
+                setSideMenuOpen(false);
+              }}
+            >
+              <IoLogOut className="mr-2 my-auto" />
+              Logout
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
