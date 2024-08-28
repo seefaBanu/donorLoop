6b8326f9e59a88af084070bca15735150d3b74a5
seefaBanu <banuags.20@uom.lk>
2024-08-28 16:21:02 +0530

    fix

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -12,7 +12,7 @@ const signInRedirectURL = import.meta.env.VITE_SIGN_IN_REDIRECT_URL;
 const signOutRedirectURL = import.meta.env.VITE_SIGN_OUT_REDIRECT_URL;
 const clientID = import.meta.env.VITE_CLIENT_ID;
 const baseUrl = import.meta.env.VITE_BASE_URL;
-const scope = import.meta.env.VITE_SCOPE.split(",");
+const scope = import.meta.env.VITE_SCOPE;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
