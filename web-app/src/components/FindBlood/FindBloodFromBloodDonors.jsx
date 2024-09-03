b4bb14a870eb5c9958f37d6b4ffd7d1762970a42
seefaBanu <banuags.20@uom.lk>
2024-09-03 15:50:02 +0530

    production url fix

--- web-app/src/components/FindBlood/FindBloodFromBloodDonors.jsx
+++ web-app/src/components/FindBlood/FindBloodFromBloodDonors.jsx
@@ -3,6 +3,16 @@ import { IoSearchCircle } from "react-icons/io5";
 import axios from "axios";
 import Spinner from "../Items/Spinner";
 import Services from "../../services/Services";
+import { IoClose } from "react-icons/io5";
+import { CiHospital1 } from "react-icons/ci";
+import { GiArrowCluster } from "react-icons/gi";
+import { GrStatusCriticalSmall } from "react-icons/gr";
+import { MdBloodtype } from "react-icons/md";
+import {
+  IoLocationSharp,
+  IoMailSharp,
+  IoDocumentTextSharp,
+} from "react-icons/io5";
 
 const FindBloodFromBloodDonors = ({ token }) => {
   const [bloodDonors, setBloodDonors] = useState([]);
@@ -11,7 +21,6 @@ const FindBloodFromBloodDonors = ({ token }) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    
     const fetchBloodDonors = async () => {
       setLoading(true);
       try {
@@ -69,108 +78,137 @@ const FindBloodFromBloodDonors = ({ token }) => {
       </div>
 
       <div className="overflow-x-auto">
-        {loading ? ( <Spinner/> ) : (
-        <div className="min-w-full">
-          {filteredBloodDonors.length > 0 ? (
-            <>
-              <div className="bg-gray-100">
-                <div className="flex justify-between bg-gray-100 items-center mt-6">
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    Name
-                  </div>
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    Blood Group
-                  </div>
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    District
-                  </div>
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    Email
-                  </div>
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    TP Number
-                  </div>
-                  <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
-                    Actions
+        {loading ? (
+          <Spinner />
+        ) : (
+          <div className="min-w-full">
+            {filteredBloodDonors.length > 0 ? (
+              <>
+                <div className="bg-gray-100">
+                  <div className="flex justify-between bg-gray-100 items-center mt-6">
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      Name
+                    </div>
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      Blood Group
+                    </div>
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      District
+                    </div>
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      Email
+                    </div>
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      TP Number
+                    </div>
+                    <div className="flex-1 text-sm font-regular text-gray-400 px-4 py-2 text-center">
+                      Actions
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                {filteredBloodDonors.map((donor) => (
-                <div
-                  key={donor.bloodDonorId}
-                  className="flex justify-between items-center my-2 bg-white rounded-xl p-2"
-                >
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    {donor.bloodDonorName}
-                  </div>
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    {donor.bloodGroups}
-                  </div>
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    {donor.district}
-                  </div>
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    {donor.email}
-                  </div>
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    {donor.tpNumber}
-                  </div>
-                  <div className="flex-1 text-sm px-4 py-2 text-center">
-                    <button
-                      className="text-blue-500 hover:underline"
-                      onClick={() => handleMoreDetailsClick(donor)}
+                <div>
+                  {filteredBloodDonors.map((donor) => (
+                    <div
+                      key={donor.bloodDonorId}
+                      className="flex justify-between items-center my-2 bg-white rounded-xl p-2"
                     >
-                      More Details
-                    </button>
-                  </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        {donor.bloodDonorName}
+                      </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        {donor.bloodGroups}
+                      </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        {donor.district}
+                      </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        {donor.email}
+                      </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        {donor.tpNumber}
+                      </div>
+                      <div className="flex-1 text-sm px-4 py-2 text-center">
+                        <button
+                          className="text-blue-500 hover:underline"
+                          onClick={() => handleMoreDetailsClick(donor)}
+                        >
+                          More Details
+                        </button>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-                ))}
+              </>
+            ) : (
+              <div className="flex justify-center items-center py-10">
+                <h2 className="text-gray-500 text-sm">
+                  No blood donors available at the moment.
+                </h2>
               </div>
-            </>
-          ) : (
-            <div className="flex justify-center items-center py-10">
-              <h2 className="text-gray-500 text-sm">
-                No blood donors available at the moment.
-              </h2>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
         )}
       </div>
 
       {selectedDonor && (
-        <div className="popup fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="popup-inner bg-white p-6 rounded-lg max-w-md w-full">
-            <h2 className="text-xl font-bold mb-4">Blood Donor Details</h2>
-            <div>
-              <p>
-                <strong>Name:</strong> {selectedDonor.bloodDonorName}
-              </p>
-              <p>
-                <strong>Blood Group:</strong> {selectedDonor.bloodGroups}
-              </p>
-              <p>
-                <strong>District:</strong> {selectedDonor.district}
-              </p>
-              <p>
-                <strong>Email:</strong> {selectedDonor.email}
-              </p>
-              <p>
-                <strong>Address:</strong> {selectedDonor.location}
-              </p>
-              <p>
-                <strong>TP Number:</strong> {selectedDonor.tpNumber}
-              </p>
+        <div
+          className="popup fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center "
+          onClick={handleClosePopup}
+        >
+            <div className="popup-inner bg-white  bg-opacity-100 shadow-inner rounded-3xl w-full backdrop-blur-sm mx-80 md:mx-10 sm:mx-5 xs:mx-5">
+              <div className=" bg-black  p-6 rounded-t-3xl flex-col justify-center ">
+                <button className=" justify-end float-end text-sm text-black hover:text-gray-700 hover:bg-gray-200 rounded-full bg-white ">
+                  <IoClose size={24} onClick={handleClosePopup} />
+                </button>
+                <h2 className="text-base font-light text-white text-center">
+                  Blood Donor Details
+                </h2>
+              </div>
+              <div className="p-6">
+                <div className="p-6  grid grid-cols-2 sm:grid-cols-none sm:gap-0 gap-4 text-sm font-semibold">
+                  <div className="grid  mb-2 flex items-center">
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">Name:</span>{" "}
+                      {selectedDonor.bloodDonorName}
+                    </div>
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">Blood Group:</span>{" "}
+                      {selectedDonor.bloodGroups}
+                    </div>
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">District:</span>{" "}
+                      {selectedDonor.district}
+                    </div>
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">Email:</span>{" "}
+                      {selectedDonor.email}
+                    </div>
+                  </div>
+                  <div className="grid mb-2 flex items-center">
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">Address:</span>{" "}
+                      {selectedDonor.location}
+                    </div>
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">TP Number:</span>{" "}
+                      {selectedDonor.tpNumber}
+                    </div>
+                    <div className="mb-2 flex items-center">
+                      <CiHospital1 className="text-gray-400 mr-2" />
+                      <span className="font-light">Donation status:</span>{" "}
+                      {selectedDonor.status}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <button
-              className="mt-4 px-4 py-2 bg-black text-white rounded-lg"
-              onClick={handleClosePopup}
-            >
-              Close
-            </button>
           </div>
-        </div>
       )}
     </div>
   );
